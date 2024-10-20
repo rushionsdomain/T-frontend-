991af565ae0249abf61d8a0376cdf66ee263b70d
Gachuri <rushionchegge@gmail.com>
2024-10-20 06:31:11 +0300

    finished 80% of frontend with all code incorporation by Ahmed & Mercy

---
+++ frontend/src/components/Sidebar/Copyright.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Copyright() {
+  return (
+    <div className="ml-5 space-y-1 border-t-2 py-2 text-xs font-normal text-gray-400">
+      <a className="cursor-pointer hover:underline">About • </a>
+      <a className="cursor-pointer hover:underline">Help • </a>
+      <a className="cursor-pointer hover:underline">Press • </a>
+      <a className="cursor-pointer hover:underline">API • </a>
+      <a className="cursor-pointer hover:underline">Jobs • </a>
+      <br />
+      <a className="cursor-pointer hover:underline">Privacy • </a>
+      <a className="cursor-pointer hover:underline">Terms • </a>
+      <a className="cursor-pointer hover:underline">Locations • </a>
+      <br />
+      <a className="cursor-pointer hover:underline">Top Accounts • </a>
+      <a className="cursor-pointer hover:underline">Hashtags • </a>
+      <a className="cursor-pointer hover:underline">Language</a>
+      <p className="pt-4">© 2022 INSTAGRAM 2.0 BY DIVYAM</p>
+      <p className="pt-4">
+        Note : This clone has been made only <br /> for educational purposes. No <br /> copyright
+        intended.
+      </p>
+    </div>
+  );
+}
+
+export default Copyright;
